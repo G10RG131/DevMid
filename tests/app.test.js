1bcd3d3219ee0b0feb2368d681bcf3ac1597a965
G10RG131 <giosharva@gmail.com>
2025-04-19 18:50:06 +0400

    test

--- tests/app.test.js
+++ tests/app.test.js
@@ -1,5 +1,5 @@
-const request = require('supertest');
-const app = require('../app');
+import request from 'supertest';
+import app from '../app';
 
 describe('Task Routes', () => {
   it('should add a task and access it by ID', async () => {
